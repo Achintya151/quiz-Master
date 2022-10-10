fedad200e6d5af5ee7c6959d2c2ca748a49d5377
Achintya151 <achintyakumartalukdar@gmail.com>
2022-10-11 00:00:57 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar bg='light' variant='light' expand="lg">
+            <Container>
+                <Navbar.Brand href='/'>
+                    Quiz Master
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='me-auto'>
+                        <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/statistics'>Statistics</Nav.Link>
+                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
